a3f1917560bb040d64532f92cb69f3f73730d59f
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-08-17 00:03:02 -0500

    yay more changes

--- routes/index.js
+++ routes/index.js
@@ -31,8 +31,57 @@ router.get('/meet-the-team', function (req, res, next) {
     evergreenvalleyteam: evergreenValleyTeam,
     leighteam: leighTeam,
     lynbrookteam:  lynbrookTeam,
-    montevistateam: montaVistaTeam,
+    montavistateam: montaVistaTeam,
     valleychristianteam: valleyChristianTeam
+  });
+});
+
+/* GET find-a-mentor page. */
+router.get('/find-a-mentor', function (req, res, next) {
+  res.render('find-a-mentor', {
+    title: 'Keystone Mentorship - Find A Mentor'
+  });
+});
+
+/* GET our-badges page. */
+router.get('/our-badges', function (req, res, next) {
+  res.render('our-badges', {
+    title: 'Keystone Mentorship - Our Badges'
+  });
+});
+
+/* GET about-us page. */
+router.get('/about-us', function (req, res, next) {
+  res.render('about-us', {
+    title: 'Keystone Mentorship - About Us'
+  });
+});
+
+/* GET faqs page. */
+router.get('/faqs', function (req, res, next) {
+  res.render('faqs', {
+    title: 'Keystone Mentorship - Frequently Asked Questions'
+  });
+});
+
+/* GET policies page. */
+router.get('/policies', function (req, res, next) {
+  res.render('policies', {
+    title: 'Keystone Mentorship - Policies'
+  });
+});
+
+/* GET our-badges page. */
+router.get('/special-thanks', function (req, res, next) {
+  res.render('special-thanks', {
+    title: 'Keystone Mentorship - Special Thanks'
+  });
+});
+
+/* GET catch-all page. */
+router.get('*', function (req, res, next) {
+  res.render('404', {
+    title: 'Keystone Mentorship - 404 - Not Found'
   });
 });
 
